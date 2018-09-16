58b5f5f01a7c49193cc2437fcd39b9ac79ccfcfa
Keyur Patel <keyurpatel6808@gmail.com>
2018-09-16 11:54:45 -0400

    Uploading complete project

---
+++ src/test/java/com/PrintStatement/test/PrintHI.java
@@ -0,0 +1,14 @@
+package com.PrintStatement.test;
+
+import org.testng.annotations.Test;
+
+
+public class PrintHI {
+	
+	@Test
+	public void MethodHi()
+	{
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/test/java/com/PrintStatement/test/PrintHello.java
@@ -0,0 +1,14 @@
+package com.PrintStatement.test;
+
+import org.testng.annotations.Test;
+
+
+public class PrintHello {
+	
+
+	@Test
+	public void MethodHello()
+	{
+		System.out.println("Hello");
+	}
+}
